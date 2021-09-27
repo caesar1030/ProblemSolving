16e4e012683da4b185215c128fa19af9d2d0eeca
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-09-28 01:48:18 +0900

    4주차

---
+++ Week4_1_12171732_WanseopPark.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	
+
+	while (t--)
+	{
+		int a, b, x, y;
+		cin >> a >> b >> x >> y;
+
+		int min;
+		int max;
+
+		min = (a < b ? a : b);
+		max = (a > b ? a : b);
+
+		int gcd = max - min;
+		int cost = 987654321;
+		int cost2 = 987654321;
+		int cost3 = 987654321;
+
+		if ((max % gcd == 0) && (min % gcd == 0)) {
+			cost = 0;
+			cout << gcd << " " << cost << "\n";
+			continue;
+		}
+
+
+		for (int i = 1; i < 1000000; i++) {
+			if (((max+i) % gcd == 0) && ((min+i) % gcd == 0)) {
+				cost2 = i * x;
+				break;
+			}
+		}
+
+		for (int i = 1; (min-i) >= gcd; i++) {
+			if (((max-i) % gcd == 0) && ((min-i) % gcd == 0)) {
+				cost3 = i * y;
+				break;
+			}
+		}
+		
+		cost = cost2 < cost3 ? cost2 : cost3;
+
+		cout << gcd << " " << cost << "\n";
+	}
+}
---
+++ Week4_2_1_12171732_WanseopPark.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
+    if (x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1 > 0) {
+        return 1;
+    }
+    else if (x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1 < 0) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        pair<int, int> a; // 선분1의 점1
+        pair<int, int> b; // 선분1의 점2
+
+        pair<int, int> c; // 선분2의 점1
+        pair<int, int> d; // 선분2의 점2
+
+        int x1, y1, x2, y2, x3, y3, x4, y4;
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+
+        a.first = x1; 
+        a.second = y1;
+        b.first = x2;
+        b.second = y2;
+       
+        c.first = x3;
+        c.second = y3;
+        d.first = x4;
+        d.second = y4;
+
+        if (a > b) {
+            swap(a, b);
+        }
+        if (c > d) {
+            swap(c, d);
+        }
+        if (a > c || (a==c &&  b < d)) {
+            swap(a, c);
+            swap(b, d);
+        }
+
+        int abc = ccw(a.first, a.second, b.first, b.second, c.first, c.second);
+        int abd = ccw(a.first, a.second, b.first, b.second, d.first, d.second);
+        int cda = ccw(c.first, c.second, d.first, d.second, a.first, a.second);
+        int cdb = ccw(c.first, c.second, d.first, d.second, b.first, b.second);
+
+        
+        if (abc * abd == 0 && cda * cdb == 0) { // 두 선이 한 직선안에 포함
+            if (b < c) {
+                cout << 1 << "\n";
+            }
+            else if (b == c) {
+                cout << 2 << "\n";
+            }
+            else if (c<b && d>b) {
+                cout << 3 << "\n";
+            }
+            else if (c < b && d <= b) {
+                cout << 4 << "\n";
+            }
+        }
+        else {
+            if (abc * abd > 0 || cda * cdb > 0) { // 한 선분을 포함하는 직선에 대해 두 점이 모두 같은 방향에 있으므로 접점x
+                cout << 1 << "\n";
+            }
+            else { // <=0 && <=0
+                cout << 2 << "\n";
+            }
+        }
+
+    }
+    
+}
