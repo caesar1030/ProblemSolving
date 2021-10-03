b23e83d6eda6103071dc3a2bee8dc44f86bf9911
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-03 20:12:11 +0900

    5주차까지

---
+++ Week5_1_12171732_WanseopPark.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[5005];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cin >> arr[i];
+		}
+
+		int len = 1;
+		int i;
+		bool flag = false;
+		
+		
+		while (!flag)
+		{
+			for (i = len+1; i <= n; i++) {
+				if (arr[i] != arr[i - len]) {
+					break;
+				}
+			}
+
+			if (i == n + 1) {
+				flag = true;
+				break;
+			}
+
+			++len;
+		}
+
+		for (int i = 1; i <= len; i++) {
+			cout << arr[i] << " ";
+		}
+
+		cout << "\n";
+	}
+}
---
+++ Week5_2_12171732_WanseopPark.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int arr[1000001];
+
+int n,m;
+int ans;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    cin >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    int left = 1;
+    int right = arr[n];
+
+   
+    while (left <= right)
+    {
+        int count = 1;
+        int mid = (left + right) / 2;
+        int latest = 1;
+
+
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] - arr[latest] >= mid) {
+                ++count;
+                latest = i;
+            }
+        }
+
+        if (count >= m) {
+            ans = mid;
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+
+    }
+  
+    cout << ans;
+}
