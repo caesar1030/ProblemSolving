f328d376fc7cf67c421cba397bb3675711d92728
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-08 22:43:06 +0900

    6주차

---
+++ Week6_1_12171732_WanseopPark.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+
+class info {
+public:
+	int idx;
+	int endTime;
+	int price;
+
+	info(int _i, int _t, int _p) {
+		idx = _i;
+		endTime = _t;
+		price = _p;
+	}
+};
+
+struct cmp {
+	bool operator() (info a, info b){
+		if (a.endTime == b.endTime) {
+			return a.idx > b.idx;
+		}
+		return a.endTime > b.endTime;
+	}
+};
+
+
+int n; // 계산대 수
+int m; // 손님 수 
+priority_queue<info, vector<info> , cmp> pq; // 계산대 인덱스
+queue<pair<int, int>> q; // 대기 {시간, 가격} 
+int now = 0;
+int totalPrice[20001]; // 총계산시간
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		q.push({ a,b });
+	}
+
+	for (int i = 0; i < n; i++) {
+		int nTime = q.front().first; 
+		int nPrice = q.front().second; 
+		q.pop();
+		pq.push(info(i, nTime, nPrice));
+	}
+
+	while (!q.empty()) {
+		int nIdx = pq.top().idx;
+		totalPrice[nIdx] += pq.top().price;
+		now = pq.top().endTime;
+		pq.pop();
+		int nTime = now + q.front().first;
+		int nPrice = q.front().second;
+		q.pop();
+		pq.push(info(nIdx, nTime, nPrice));
+	}
+
+	while (!pq.empty()) {
+		int nIdx = pq.top().idx;
+		totalPrice[nIdx] += pq.top().price;
+		now = pq.top().endTime;
+		pq.pop();
+	}
+
+	cout << now << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << totalPrice[i] << "\n";
+	}
+
+}
---
+++ Week6_2_12171732_WanseopPark.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int arr[1001][1001];
+bool check[1001][1001];
+int x[8] = { -1,-1,-1,0,1,1,1,0 };
+int y[8] = { 1,0,-1,-1,-1,0,1,1 };
+int n;
+
+int main()
+{
+    int t;
+	cin >> t;
+	while (t--)
+	{
+		for (int i = 0; i < 1001; i++) {
+			for (int j = 0; j < 1001; j++) {
+				arr[i][j] = 0;
+				check[i][j] = false;
+			}
+		}
+
+
+		cin >> n;
+		
+		int a, b, c, d; // 인하 a,b 안전지역 c,d
+		cin >> a >> b >> c >> d;
+		
+		int e, f, g, h; // 소총수 e,f 검사 g,h
+		cin >> g >> h >> e >> f;
+		
+
+		check[e][f] = true;
+		check[g][h] = true;
+
+		int sof = f;
+		int soe = e;
+
+
+		
+		for (int i = 0; i < 8; i++) {
+			//소총수
+			while ((soe >= 1 && soe <= n) && (sof >= 1 && sof <= n)) {
+				sof += x[i];
+				soe += y[i];
+
+				if (arr[soe][sof]  == false) {
+					check[soe][sof] = true;
+				}
+				else {
+					break;
+				}
+			}
+			sof = f;
+			soe = e;
+			
+			// 검사
+			if (g + y[i] >= 0 && g + y[i] <= n && h + x[i] >= 0 && h + x[i] <= n) {
+				check[g + y[i]][h + x[i]] = true;
+			}
+		}
+
+		queue<pair<int, int>> q;
+
+		q.push({ a,b });
+		check[a][b] = true;
+
+		while (!q.empty()) {
+			int row = q.front().first;
+			int col = q.front().second;
+			q.pop();
+
+			for (int i = 0; i < 8; i++) {
+				int nrow = row + y[i];
+				int ncol = col + x[i];
+				if (nrow >= 1 && nrow <= n && ncol >= 1 && ncol <= n) {
+					if (check[nrow][ncol] == false) {
+						q.push({ nrow,ncol });
+						check[nrow][ncol] = true;
+						arr[nrow][ncol] = arr[row][col] + 1;
+					}
+				}
+			}
+		}
+
+		cout << arr[c][d] << "\n";
+
+	}
+}
