0aaf9d99fee13bbb1718a074551cdc188bfd0bff
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-20 22:02:56 +0900

    7주차

---
+++ Week7_1_12171732_WanseopPark.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        vector<int> former[101];
+        
+        
+        int ans[101] = { 0, };
+        int n; // 공 개수
+        int m; // 비교 횟수
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+            former[b].push_back(a);
+            
+        }
+
+        for (int i = 1; i <= n; i++ ) {
+            bool check[101] = { 0, };
+            queue<int> q;
+            q.push(i);
+
+            while (!q.empty()) {
+                int now = q.front(); q.pop();
+                for (auto x : former[now]) {
+                    if (!check[x]) {
+                        ans[x] += 1;
+                        q.push(x);
+                        check[x] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            cout << ans[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
---
+++ Week7_2_12171732_WanseopPark.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+class info {
+public:
+    
+    pair<int, int> key;
+    info(pair<int, int> _k) {
+        key.first = _k.first;
+        key.second = _k.second;
+    }
+};
+
+struct cmp {
+    bool operator() (info a, info b) {
+        if (a.key.first * a.key.first + a.key.second * a.key.second == b.key.first * b.key.first + b.key.second * b.key.second) {
+            return a.key < b.key;
+        }
+        return a.key.first * a.key.first + a.key.second * a.key.second < b.key.first * b.key.first + b.key.second * b.key.second;
+    }
+};
+
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int m; // 가게 수
+        int n; // 날짜
+        int k; // 가까운 순서 갈집
+
+        cin >> m >> n >> k;
+
+        priority_queue<info, vector<info>, cmp> pq;
+        
+
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            cin >> x >> y;
+            pq.push(info({ x, y }));
+        }
+
+        while (pq.size() > k) {
+            pq.pop();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int x, y;
+            cin >> x >> y;
+            pair<int, int> tmp = { x,y };
+
+            if (pq.top().key.first * pq.top().key.first + pq.top().key.second * pq.top().key.second > tmp.first * tmp.first + tmp.second * tmp.second) {
+                pq.pop();
+                pq.push(tmp);
+            }
+            else if (pq.top().key.first * pq.top().key.first + pq.top().key.second * pq.top().key.second == tmp.first * tmp.first + tmp.second * tmp.second
+                && tmp < pq.top().key) {
+                pq.pop();
+                pq.push(tmp);
+            }
+            
+
+            cout << pq.top().key.first << " " << pq.top().key.second  << "\n";
+        }
+    }
+}
